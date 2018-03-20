a02acaf47514b078704d535f0af8640d28a294d2
julien boyer <juh@macbook-pro-de-julien.home>
2018-03-20 09:04:21 +0100

    Add heuristics methods

--- Assets/Scripts/PathFinder.cs
+++ Assets/Scripts/PathFinder.cs
@@ -3,6 +3,9 @@ using System.Collections.Generic;
 
 public class PathFinder {
 
+    public float D = 1; // The heuristic constant applied on computation for a minimum normal cost
+    public float D2 = Mathf.Sqrt(2); // Cost for diagonal heuristics (sqrt(2) -> octile distance)
+
     Node Start;
     Node End;
 
@@ -19,5 +22,24 @@ public class PathFinder {
 
     public Node[] Compute() {
         return new Node[5];
+    }
+
+    // ===========================
+    //         Heuristics
+    // ===========================
+
+    private double ManathanDistance(Node nodeA, Node nodeB) {
+        double distX = nodeA.GridIndexes.x - nodeB.GridIndexes.x;
+        double distY = nodeA.GridIndexes.y - nodeB.GridIndexes.y;
+
+        return D * (distX * distY);
+    }
+
+
+    private double DiagManathan(Node nodeA, Node nodeB) {
+        float distX = nodeA.GridIndexes.x - nodeB.GridIndexes.x;
+        float distY = nodeA.GridIndexes.y - nodeB.GridIndexes.y;
+
+        return D * (distX + distY) + (D2 - 2 * D) * Mathf.Min(distX, distY);
     }
 }
